3fa7c31299c5c597ffcc02ef6e27255ad52748ac
Ray Goodwin <rgoodwin16@outlook.com>
2015-07-19 17:12:09 -0400

    7/19/15
    Yay got User Roles to work so that certain links only show up when the
    current user is logged in in the correct role.

--- WebApplication1/WebApplication1/Models/BlogPost.cs
+++ WebApplication1/WebApplication1/Models/BlogPost.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.Web.Mvc;
+    using Microsoft.AspNet.Identity;
 
     public class BlogPost
     {
